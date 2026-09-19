e2242ec82a100537f56d96f96e0f0bd7ca7e8625
Arnav Anant <arnavanant04@gmail.com>
2026-09-20 01:10:30 +0530

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        
+        int[][] cars = new int[position.length][2];
+
+        for(int i = 0 ; i < position.length ; i++ ){
+             cars[i][0] = position[i];
+             cars[i][1] = speed[i];
+        }
+        
+        Arrays.sort(cars,(a,b) -> Integer.compare(b[0],a[0]));
+        
+        Stack<Double> stack = new Stack<>();
+        int ans = position.length;
+
+        for(int i = 0 ; i < cars.length ; i++){
+             
+            double time = (double)(target - cars[i][0]) / cars[i][1];
+
+            if(stack.empty() || stack.peek() < time){
+                stack.push(time);
+            }
+            
+        }
+        
+        return stack.size();
+    }
+}
